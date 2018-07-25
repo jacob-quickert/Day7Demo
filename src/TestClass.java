638dedc4575059bbce3f418f30a4f662b60c6912
jacob <jacob.quickert@gmail.com>
2018-07-25 09:35:22 -0400

    second commit: added Test.java for github

---
+++ src/TestClass.java
@@ -0,0 +1,11 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is just a test for github...");
+		System.out.println("hello world");
+
+	}
+
+}
